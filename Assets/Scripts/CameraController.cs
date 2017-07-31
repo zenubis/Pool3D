9ec20a516617eff42e26134b9e466f69f5247e67
Nyon <yowfeng.nyon@gmail.com>
2017-08-01 01:02:27 +0800

    first commit with crude, 3d primitives model of pool table

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float minZoom = 1;
+    public float maxZoom = 5;
+
+    private GameObject cueBall;
+    private float currentZoom = 1.0f;
+	
+	void Start ()
+    {
+        cueBall = GameObject.Find("Ball");
+        
+        Vector3 lookAt = cueBall.transform.position;
+        lookAt.y = 0;
+
+        Vector3 viewDir = new Vector3(1, 1, 1);
+        viewDir.Normalize();
+        
+        transform.position = lookAt + viewDir * currentZoom;
+        transform.LookAt(lookAt);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+
+        }
+
+        // 
+        //Input.GetAxis("Mouse ScrollWheel");
+	}
+
+    
+}
