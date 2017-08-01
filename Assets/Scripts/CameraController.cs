8c430693908b39efd4adf673b2d53bd638def292
Nyon <yowfeng.nyon@gmail.com>
2017-08-01 22:59:14 +0800

    add some basic camera control code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,36 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    public float minZoom = 1;
-    public float maxZoom = 5;
+    public float minZoom = 0.8f;
+    public float maxZoom = 3;
 
-    private GameObject cueBall;
-    private float currentZoom = 1.0f;
+    private GameObject whiteBall;
+    private Rigidbody whiteBallRB;
+
+    // camera will be controlled using these variables
+    private float cameraZoom = 1.0f;
+    private float cameraAngle = 0; // on the xz plane
+    private Vector3 cameraLookAt = new Vector3(0, 0, 0);
 	
 	void Start ()
     {
-        cueBall = GameObject.Find("Ball");
-        
-        Vector3 lookAt = cueBall.transform.position;
-        lookAt.y = 0;
+        whiteBall = GameObject.Find("WhiteBall");
+        if (null != whiteBall) {
+            whiteBallRB = whiteBall.GetComponent<Rigidbody>();
+        }
 
-        Vector3 viewDir = new Vector3(1, 1, 1);
-        viewDir.Normalize();
-        
-        transform.position = lookAt + viewDir * currentZoom;
-        transform.LookAt(lookAt);
-	}
+        cameraLookAt = whiteBall.transform.position;
+        cameraLookAt.y = 0;
+
+        // update camera rotation
+        UpdateCameraTransformation();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+        bool updateCameraTransform = false;
 
+        float wheelScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (wheelScroll != 0) {
+            cameraZoom += wheelScroll * 0.5f;
+            cameraZoom = Mathf.Clamp(cameraZoom, minZoom, maxZoom);
+            updateCameraTransform = true;
         }
 
-        // 
-        //Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetMouseButton(1)) {
+            // if the right mouse button is down, we rotate the camera around
+            cameraAngle += Input.GetAxis("Mouse X");
+
+            // keep the value of cameraAngle from -360 to 360
+            cameraAngle -= (int)(cameraAngle / 360) * 360;
+            updateCameraTransform = true;
+        }
+
+        if (updateCameraTransform) {
+            UpdateCameraTransformation();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            // hit the ball!
+            if (null != whiteBallRB) {
+                whiteBallRB.AddForce(GetCameraToBallVector() * 200);
+            }
+        }
+
+        Debug.Log("whiteBallRB.velocity" + whiteBallRB.velocity.ToString());
 	}
 
+    private void UpdateCameraTransformation()
+    {
+        Vector3 viewDir = new Vector3(1, 0, 0);
+        Quaternion rotate = Quaternion.Euler(0, cameraAngle, 30);
+        viewDir = rotate * viewDir;
+        Debug.Log("cameraAngle:" + cameraAngle); 
+        //Debug.Log("cameraZoom:" + cameraZoom + " viewDir:" + viewDir.ToString());
+        transform.position = cameraLookAt + viewDir * cameraZoom;
+        transform.LookAt(cameraLookAt);
+    }
+
+    private Vector3 GetCameraToBallVector()
+    {
+        Vector3 dir = whiteBall.transform.position - transform.position;
+        dir.y = 0;
+        dir.Normalize();
+        return dir;
+    }
     
 }
